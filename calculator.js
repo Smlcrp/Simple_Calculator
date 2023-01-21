bd237fcc91760d11554337228c198c87b4412f04
Sam Carp <smlcrp@gmail.com>
2023-01-21 12:43:10 -0500

    linked javascript file properly

--- calculator.js
+++ calculator.js
@@ -1,20 +1,18 @@
-<script>
-  var num1, num2, result;
-  function performOperation(operation) {
-    num1 = document.getElementById("num1").value;
-    num2 = document.getElementById("num2").value;
-    num1 = parseFloat(num1);
-    num2 = parseFloat(num2);
-    if (operation == "add") {
-      result = num1 + num2;
-    } else if (operation == "subtract") {
-      result = num1 - num2;
-    } else if (operation == "multiply") {
-      result = num1 * num2;
-    } else if (operation == "divide") {
-      result = num1 / num2;
-    }
-    document.getElementById("result").innerHTML = result;
+var num1, num2, result;
+function performOperation(operation) {
+  num1 = document.getElementById("num1").value;
+  num2 = document.getElementById("num2").value;
+  num1 = parseFloat(num1);
+  num2 = parseFloat(num2);
+  if (operation == "add") {
+    result = num1 + num2;
+  } else if (operation == "subtract") {
+    result = num1 - num2;
+  } else if (operation == "multiply") {
+    result = num1 * num2;
+  } else if (operation == "divide") {
+    result = num1 / num2;
   }
-</script>
-
+  document.getElementById("result").innerHTML = result;
+  alert("Congratulations on using the calculator! Your answer is " + result);
+}
