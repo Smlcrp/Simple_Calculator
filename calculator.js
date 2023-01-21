2e4615fa8a3e8844ae5fc2037a48ac6a0e4cb2a0
Sam Carp <smlcrp@gmail.com>
2023-01-21 12:30:35 -0500

    Initial commit

---
+++ calculator.js
@@ -0,0 +1,20 @@
+<script>
+  var num1, num2, result;
+  function performOperation(operation) {
+    num1 = document.getElementById("num1").value;
+    num2 = document.getElementById("num2").value;
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if (operation == "add") {
+      result = num1 + num2;
+    } else if (operation == "subtract") {
+      result = num1 - num2;
+    } else if (operation == "multiply") {
+      result = num1 * num2;
+    } else if (operation == "divide") {
+      result = num1 / num2;
+    }
+    document.getElementById("result").innerHTML = result;
+  }
+</script>
+
